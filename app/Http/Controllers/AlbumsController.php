c916c27dc537d91f5a80a0b8026a69f1290807b5
MbIWaTUK <53084176+MbIWaTUK@users.noreply.github.com>
2020-02-06 12:14:35 +0400

    Update AlbumsController.php

--- app/Http/Controllers/AlbumsController.php
+++ app/Http/Controllers/AlbumsController.php
@@ -10,6 +10,12 @@ use Illuminate\Support\Facades\URL;
 class AlbumsController extends Controller
 {
 
+    
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
     public function albums() {
         $albums = Album::paginate(3);
         $pages = $albums->lastPage();
